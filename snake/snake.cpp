52c553d349727241273f7754ab6799b57a53e7a7
chan EASON <1181499949@qq.com>
2019-05-29 18:26:49 +0800

    更新

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 
-#define winw 1000
-#define winh 500
-#define background RGB(0xE0, 0xEE, 0xE0)
-#define finfo "userinfo.txt"
+#define winw 1000 //窗口宽度
+#define winh 500 //窗口高度
+#define background RGB(0xE0, 0xEE, 0xE0) //背景色
+#define finfo "userinfo.txt" //用户信息保存文件
 
+//保存用户信息
 typedef struct USER {
 	char username[11];
 	char password[11];
@@ -18,18 +19,26 @@ typedef struct USER {
 }user;
 user usr;
 
+//蛇的每节身体
 typedef struct SNAKE {
 	int x;
 	int y;
+	int w;
+	int h;
+	struct SNAKE *next;
 }snake;
 snake snak;
 
+//食物
 typedef struct FOOD {
 	int x;
 	int y;
+	int w;
+	int h;
 }food;
 food fod;
 
+//字符串图形
 typedef struct STRING {
 	const char *tstr;
 	int x, y;
@@ -39,8 +48,8 @@ typedef struct STRING {
 
 bool menu(void);
 void setfont(void);
-int mousehandle(string *str[], int num);
-string *inittext(const char *str, int pianx, int piany);
+int mousehandle(string *str, int num);
+string inittext(const char *str, int pianx, int piany);
 void about(void);
 void score(void);
 user *readinfo(void);
@@ -53,9 +62,9 @@ RECT windows;
 int main(void)
 {
 	initgraph(winw, winh);	// 创建绘图窗口
-	setbkcolor(RGB(0xE0, 0xEE, 0xE0)); //设置背景
+	//setbkcolor(background); //设置背景
 	cleardevice();	//刷新绘图窗口
-	RECT windows = { 0, 0, 1000, 500 }; //保存窗口信息
+	windows = { 0, 0, 1000, 500 }; //保存窗口信息
 	setfont();	//设置字体
 	while (true)//菜单
 	{
@@ -68,33 +77,45 @@ int main(void)
 	return 0;
 }
 
+/*
+函数功能：绘制菜单
+函数参数：无
+函数返回值：是否退出程序
+*/
 bool menu(void)
 {
-	string *str[4];
+	string str[4],title;
 	int ichoise, i;
 
 	cleardevice();
-	str[0] = inittext("开始游戏", 0, -120);//初始化字体
-	str[1] = inittext("排行榜", 0, -40);
-	str[2] = inittext("关于作者", 0, 40);
-	str[3] = inittext("退出游戏", 0, 120);
+	settextstyle(80, 0, "微软雅黑");
+	title = inittext("贪吃蛇", -90, -200);
+	outtextxy(title.x, title.y, title.tstr);
+	settextstyle(50, 0, "微软雅黑");
+	title = inittext("—无尽版", 90, -190);
+	outtextxy(title.x, title.y, title.tstr);
+	str[0] = inittext("开始游戏", 0, -100);//初始化字体
+	str[1] = inittext("排行榜", 0, -20);
+	str[2] = inittext("关于作者", 0, 60);
+	str[3] = inittext("退出游戏", 0, 140);
 	for (i = 0; i < 4; i++)
 	{
-		outtextxy(str[i]->x, str[i]->y, str[i]->tstr);//生成字体
+		outtextxy(str[i].x, str[i].y, str[i].tstr);
 	}
 	ichoise = mousehandle(str, 4); //鼠标事件
 	switch (ichoise)
 	{
 	case 0:
+		//开始游戏
 		break;
 	case 1:
-		score();
+		score();//排行榜
 		break;
 	case 2:
-		about();
+		about();//关于作者
 		break;
 	case 3:
-		return true;
+		return true;//退出游戏
 		break;
 	default:
 		break;
@@ -102,6 +123,11 @@ bool menu(void)
 	return false;
 }
 
+/*
+函数功能：登录
+函数参数：
+函数返回值：
+*/
 bool login(void)
 {
 	if (InputBox(usr.username, 11, "请输入用户名", "请登录", 0, 0, 0, false)) //创建inputbox输入用户信息
@@ -111,6 +137,11 @@ bool login(void)
 	return false;
 }
 
+/*
+函数功能：初始化输出的字符串图形的字体
+函数参数：无
+函数返回值：无
+*/
 void setfont(void)
 {
 	LOGFONT font;
@@ -120,10 +151,15 @@ void setfont(void)
 	_tcscpy_s(font.lfFaceName, "微软雅黑");    // 设置字体
 	font.lfQuality = ANTIALIASED_QUALITY;    // 设置输出效果为抗锯齿  
 	settextstyle(&font);					// 设置字体样式
-	settextcolor(RGB(0x8b, 0x2e, 0x3f));	//设置字体颜色
+	settextcolor(RGB(0x7F, 0xFF, 0x00));	//设置字体颜色
 }
 
-int mousehandle(string *str[], int num)
+/*
+函数功能：处理鼠标事件
+函数参数：待点击的字符串头指针 以及字符串数目
+函数返回值：点击的字符串的ID
+*/
+int mousehandle(string *str, int num)
 {
 	MOUSEMSG mouse;
 	int i;
@@ -134,12 +170,12 @@ int mousehandle(string *str[], int num)
 		{
 			mouse = GetMouseMsg();
 			//如果鼠标在文字上面则加大字号
-			if (mouse.x >= str[i]->x && mouse.x <= str[i]->x + str[i]->w && mouse.y >= str[i]->y && mouse.y <= str[i]->y + str[i]->h)
+			if (mouse.x >= str[i].x && mouse.x <= str[i].x + str[i].w && mouse.y >= str[i].y && mouse.y <= str[i].y + str[i].h)
 			{
 				settextstyle(60, 0, "微软雅黑");
-				str[i] = inittext(str[i]->tstr, str[i]->pianx, str[i]->piany);
-				outtextxy(str[i]->x, str[i]->y, str[i]->tstr);
-				while ((mouse.x >= str[i]->x && mouse.x <= str[i]->x + str[i]->w && mouse.y >= str[i]->y && mouse.y <= str[i]->y + str[i]->h))
+				str[i] = inittext(str[i].tstr, str[i].pianx, str[i].piany);
+				outtextxy(str[i].x, str[i].y, str[i].tstr);
+				while ((mouse.x >= str[i].x && mouse.x <= str[i].x + str[i].w && mouse.y >= str[i].y && mouse.y <= str[i].y + str[i].h))
 				{
 					mouse = GetMouseMsg();
 					if (mouse.mkLButton)//点击鼠标左键返回id
@@ -149,36 +185,46 @@ int mousehandle(string *str[], int num)
 					}
 				}
 				//鼠标移出文字区域，刷新背景
-				clearrectangle(str[i]->x, str[i]->y, str[i]->x + str[i]->w, str[i]->y + str[i]->h);
+				clearrectangle(str[i].x, str[i].y, str[i].x + str[i].w, str[i].y + str[i].h);
 			}
 			else
 			{
 				//重设为默认字体
 				settextstyle(50, 0, "微软雅黑");
-				str[i] = inittext(str[i]->tstr, str[i]->pianx, str[i]->piany);
-				outtextxy(str[i]->x, str[i]->y, str[i]->tstr);
+				str[i] = inittext(str[i].tstr, str[i].pianx, str[i].piany);
+				outtextxy(str[i].x, str[i].y, str[i].tstr);
 			}
 		}
 	}
 }
 
-string *inittext(const char *str, int pianx, int piany)
+/*
+函数功能：将字符串转化为图像信息
+函数参数：待转字符串 相对于屏幕中央的偏移
+函数返回值：字符串图形结构体
+*/
+string inittext(const char *str, int pianx, int piany)
 {
-	string *sstr;
+	string sstr;
 
-	sstr->tstr = str;
-	sstr->x = (winw - textwidth(str)) / 2 + pianx;
-	sstr->y = (winh - textheight(str)) / 2 + piany;
-	sstr->w = textwidth(str);
-	sstr->h = textheight(str);
-	sstr->pianx = pianx;
-	sstr->piany = piany;
+	sstr.tstr = str;
+	sstr.x = (winw - textwidth(str)) / 2 + pianx;
+	sstr.y = (winh - textheight(str)) / 2 + piany;
+	sstr.w = textwidth(str);
+	sstr.h = textheight(str);
+	sstr.pianx = pianx;
+	sstr.piany = piany;
 	return sstr;
 }
 
+/*
+函数功能：绘制关于界面
+函数参数：无
+函数返回值：无
+*/
 void about(void)
 {
-	string *about[3];
+	string about[3];
 	IMAGE alipay, wenxinpay;
 
 	cleardevice();
@@ -189,11 +235,11 @@ void about(void)
 		putimage(40, 100, &alipay);
 		putimage(winw - 40 - 240, 100, &wenxinpay);
 		about[0] = inittext("支持作者", 0, -120);
-		outtextxy(about[0]->x, about[0]->y, about[0]->tstr);
+		outtextxy(about[0].x, about[0].y, about[0].tstr);
 		about[1] = inittext("一个吃土的程序猿", 0, -40);
-		outtextxy(about[1]->x, about[1]->y, about[1]->tstr);
+		outtextxy(about[1].x, about[1].y, about[1].tstr);
 		about[2] = inittext("返回", 0, 120);
-		outtextxy(about[2]->x, about[2]->y, about[2]->tstr);
+		outtextxy(about[2].x, about[2].y, about[2].tstr);
 		if (mousehandle(about + 2, 1) == 0)
 		{
 			break;
@@ -201,13 +247,19 @@ void about(void)
 	}
 }
 
+/*
+函数功能：输出排行榜
+函数参数：无
+函数返回值：无
+*/
 void score(void)
 {
 	user *head = NULL, *pp = NULL;
 	char *tmp = NULL;
 	unsigned int totlesize = 0, usedsize = 0;
-	string *title[5];//抬头
-	string *data[10][5];//十个用户的信息
+	string title[5];//抬头
+	string data[10][5];//十个用户的信息
+	string tips, back;
 	int i, j, ilen = 0;
 
 	cleardevice();
@@ -218,52 +270,67 @@ void score(void)
 	title[4] = inittext("名次", -winw / 2 + 800 + 10, -winh / 2);
 	for (i = 0; i < 5; i++)
 	{
-		outtextxy(title[i]->x + title[i]->w / 2, title[i]->y + title[i]->h / 2, title[i]->tstr);
+		outtextxy(title[i].x + title[i].w / 2, title[i].y + title[i].h / 2, title[i].tstr);
 	}
 	head = readinfo();
 	pp = head;
 
 	settextstyle(40, 0, "微软雅黑");
+	settextcolor(RGB(0x8b, 0x2e, 0x3f));
 	while (pp != NULL)
-	{
-		for (i = 0; i < 2; i++)
-		{	//账号
-			data[i][0] = inittext(pp->username, -winw / 2 + 10, -winh / 2 + 100 + i * 50);
+	{	
+		if (pp->order <= 10)
+		{
+			//账号
+			data[i][0] = inittext(pp->username, -winw / 2 + 10, -winh / 2 + 80 + (pp->order - 1) * 40);
 			//游玩次数
+			tmp = NULL;
 			totlesize = 0;
 			usedsize = 0;
 			tmp = myintcat(tmp, &usedsize, &totlesize, pp->playcount);
-			data[i][1] = inittext(tmp, -winw / 2 + 200 + 10, -winh / 2 + 100 + i * 50);
+			data[i][1] = inittext(tmp, -winw / 2 + 200 + 10, -winh / 2 + 80 + (pp->order - 1) * 40);
 			//游玩时间
+			tmp = NULL;
 			totlesize = 0;
 			usedsize = 0;
 			tmp = myintcat(tmp, &usedsize, &totlesize, pp->timeh);
 			tmp = mystrcat(tmp, &usedsize, &totlesize, "时");
 			tmp = myintcat(tmp, &usedsize, &totlesize, pp->timem);
 			tmp = mystrcat(tmp, &usedsize, &totlesize, "分");
-			data[i][2] = inittext(tmp, -winw / 2 + 400 +10, -winh / 2 + 100 + i * 50);
+			data[i][2] = inittext(tmp, -winw / 2 + 400 + 10, -winh / 2 + 80 + (pp->order - 1) * 40);
 			//最高成绩
+			tmp = NULL;
 			totlesize = 0;
 			usedsize = 0;
 			tmp = myintcat(tmp, &usedsize, &totlesize, pp->maxscore);
-			data[i][3] = inittext(tmp, -winw / 2 + 600 + 10, -winh / 2 + 100 + i * 50);
+			data[i][3] = inittext(tmp, -winw / 2 + 600 + 10, -winh / 2 + 80 + (pp->order - 1) * 40);
 			//排名
+			tmp = NULL;
 			totlesize = 0;
 			usedsize = 0;
 			tmp = myintcat(tmp, &usedsize, &totlesize, pp->order);
-			data[i][4] = inittext(tmp, -winw / 2 + 800 + 10, -winh / 2 + 100 + i * 50);
+			data[i][4] = inittext(tmp, -winw / 2 + 800 + 10, -winh / 2 + 80 + (pp->order - 1) * 40);
 			for (j = 0; j < 5; j++)
 			{
-				outtextxy(data[i][j]->x + data[i][j]->w / 2, data[i][j]->y, data[i][j]->tstr);
-				getchar();
+				outtextxy(data[i][j].x + data[i][j].w / 2, data[i][j].y, data[i][j].tstr);
 			}
-			pp = pp->next;
 		}
-		getchar();
+		pp = pp->next;
 	}
-	
+	settextcolor(RGB(0x7F, 0xFF, 0x00));
+	tips = inittext("Tips:只显示前十名", 0, -winh / 2 + 80 + 400);
+	outtextxy(tips.x, tips.y, tips.tstr);
+	settextstyle(50, 0, "微软雅黑");
+	back = inittext("返回", -winw / 2 + 920, -winh / 2 + 80 + 9 * 40);
+	outtextxy(back.x, back.y, back.tstr);
+	mousehandle(&back, 1);
 }
 
+/*
+函数功能：从文件读取用户信息
+函数参数：无
+函数返回值：用户信息结构体
+*/
 user *readinfo(void)
 {
 	FILE *fp;
@@ -294,6 +361,11 @@ user *readinfo(void)
 	return head;
 }
 
+/*
+函数功能：写字符串到指定地址
+函数参数：待写入内存首地址 已使用空间 总空间 待加入字符串
+函数返回值：
+*/
 char *mystrcat(char *tmp, unsigned int *usedsize, unsigned int *totlesize, const char *cstr)
 {
 	if (*usedsize + strlen(cstr) + 1 > *totlesize)//内存不够
@@ -306,12 +378,17 @@ char *mystrcat(char *tmp, unsigned int *usedsize, unsigned int *totlesize, const
 	return tmp;
 }
 
+/*
+函数功能：将int转为字符串并写入到指定地址
+函数参数：待写入内存首地址 已使用空间 总空间 待加入int
+函数返回值：
+*/
 char *myintcat(char *tmp, unsigned int *usedsize, unsigned int *totlesize, unsigned int iInt)
 {
 	int ilen;
 
 	for (ilen = 1; (iInt / (int)pow(10, ilen) != 0); ilen++) { ; }//数字的位数
-	if (*usedsize + ilen + 1> *totlesize) //内存不够
+	if (*usedsize + ilen + 1 > *totlesize) //内存不够
 	{
 		*totlesize = *usedsize + ilen + 1;
 		tmp = (char *)realloc(tmp, *totlesize);
